a813bc16fc09f65e2cf5c0f7bf8cf3f667c2125b
AnonymousWhizzer <Anonymouswhizzer@proton.me>
2025-11-25 18:26:59 +0100

    Update CalculatorService.js

--- CalculatorService.js
+++ CalculatorService.js
@@ -4,7 +4,6 @@ var request = require('sync-request');
 const PORT = 80;
 const service_ip = '10.10.10.100';
 
-
 const SUM_SERVICE_IP_PORT = 'http://'+service_ip+':31001';
 const SUB_SERVICE_IP_PORT = 'http://'+service_ip+':31002';
 const MUL_SERVICE_IP_PORT = 'http://'+service_ip+':31003';
